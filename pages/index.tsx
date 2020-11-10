ac4992291470992e96f9c087d6891a83abe9d5bf
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2020-11-11 00:46:38 +0100

    Search logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,23 +7,33 @@ export default function Page() {
     const [hideOperators, setHideOperators] = React.useState(false);
     const [searchText, setSearchText] = React.useState('');
 
-    const searchResolver = (text: String) => {
+    // Resolve clicks on operators
+    const clickOperatorResolver = (index: number) => {
+        setSelectedItem(index);
+        setSearchText(operators[index].operator);
+    };
+
+    // Resolve search
+    const searchResolver = (text: string) => {
         const foundObject = operators.findIndex((operator) => operator.operator === text);
         if(foundObject !== -1) {
             setSelectedItem(foundObject);
-            setHideOperators(true);
+
+            //setHideOperators(true);
         }
         else {
             setSelectedItem(-1);
-            setHideOperators(false);
+            //setHideOperators(false);
         }
+
+        setSearchText(text);
     };
 
     const generatedItems = operators.map((feature, index) => {
 
         if(index === selectedItem) {
             return (
-                <div className="p-2 cursor-pointer" key={index} onClick={() => setSelectedItem(index)}>
+                <div className="p-2 cursor-pointer" key={index} onClick={() => clickOperatorResolver(index)}>
                     <div className="inline-flex items-center bg-purple-600 leading-none text-white rounded-full p-2 shadow text-teal text-sm">
                         <span className="text-lg inline-flex bg-white text-purple-600 rounded-full h-8 px-5 justify-center items-center">
                             {feature.operator}
@@ -37,7 +47,7 @@ export default function Page() {
         }
         else {
             return (
-                <div className="p-2 cursor-pointer" key={index} onClick={() => setSelectedItem(index)}>
+                <div className="p-2 cursor-pointer" key={index} onClick={() => clickOperatorResolver(index)}>
                     <div className="inline-flex items-center bg-white leading-none text-purple-600 rounded-full p-2 shadow text-teal text-sm">
                         <span className="text-lg inline-flex bg-purple-600 text-white rounded-full h-8 px-5 justify-center items-center">
                             {feature.operator}
@@ -59,18 +69,20 @@ export default function Page() {
                 </div>
                 <div className="text-white font-bold rounded-lg border p-2">
                     <div className="relative text-gray-600">
-                        <input onChange={(event) => searchResolver(event.target.value)} type="search" name="search" placeholder="Search" className="bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none" />
+                        <input value={searchText || ''} onChange={(event) => searchResolver(event.target.value)} type="search" name="search" placeholder="Search" className="bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none" />
                         <button type="submit" className="absolute right-0 top-0 mt-2 mr-3">🔍</button>
                     </div>
                 </div>
-                { !hideOperators ? <div className="flex flex-row m-5 flex-wrap mb-auto h-grow">
-                    {generatedItems}
-                </div> : '' }
-                { selectedItem !== -1 ? <div className="rounded-lg border p-5 m-5 w-11/12 max-w-3xl">
-                    <h2 className="text-4xl mb-2">{operators[selectedItem].name}</h2>
-                    {<ReactMarkdown>{operators[selectedItem].description}</ReactMarkdown>}
-                </div> : '' }
-                <div className="block m-2 mb-4 text-center h-10">
+                <div className="flex-grow">
+                    { !hideOperators ? <div className="flex flex-row m-5 flex-wrap mb-auto h-grow">
+                        {generatedItems}
+                    </div> : '' }
+                    { selectedItem !== -1 ? <div className="rounded-lg border p-5 m-5 w-11/12 max-w-3xl">
+                        <h2 className="text-4xl mb-2">{operators[selectedItem].name}</h2>
+                        {<ReactMarkdown>{operators[selectedItem].description}</ReactMarkdown>}
+                    </div> : '' }
+                </div>
+                <div className="block m-2 mb-2 text-center h-10">
                 Built with 💜 by <a className="text-purple-600" href="https://khromov.se" target="_blank">Stanislav Khromov</a> using <a className="text-purple-600" href="https://alephjs.org/" target="_blank">Aleph.js</a> | <a className="text-purple-600" href="/api/operators">API</a> | <a className="text-purple-600" href="https://github.com/khromov/aleph-php-operator-lookup" target="_blank">GitHub</a>
                 </div>
             </div>
