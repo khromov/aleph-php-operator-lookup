0cae25bd2c2ec4278a3b6d5975bf87cc4a69fb6b
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2020-11-11 00:32:31 +0100

    Data, logic, styling

--- operators.ts
+++ operators.ts
@@ -1,12 +1,54 @@
 export default [
     {
+        'operator': '+=',
+        'version': '',
+        'type': 'Assignment Operators',
+        'url': 'https://www.php.net/manual/en/language.operators.assignment.php',
+        'name': 'Addition',
+        'description': 'Equivalent to `$a = $a + b`'
+    },
+    {
+        'operator': '-=',
+        'version': '',
+        'type': 'Assignment Operators',
+        'url': 'https://www.php.net/manual/en/language.operators.assignment.php',
+        'name': 'Subtraction',
+        'description': 'Equivalent to `$a = $a - $b`'
+    },
+    {
+        'operator': '*=',
+        'version': '',
+        'type': 'Assignment Operators',
+        'url': 'https://www.php.net/manual/en/language.operators.assignment.php',
+        'name': 'Multiplication',
+        'description': 'Equivalent to `$a = $a * $b`'
+    },
+    {
+        'operator': '/=',
+        'version': '',
+        'type': 'Assignment Operators',
+        'url': 'https://www.php.net/manual/en/language.operators.assignment.php',
+        'name': 'Division',
+        'description': 'Equivalent to `$a = $a / $b`'
+    },
+    {
+        'operator': '%=',
+        'version': '',
+        'type': 'Assignment Operators',
+        'url': 'https://www.php.net/manual/en/language.operators.assignment.php',
+        'name': 'Modulus',
+        'description': 'Equivalent to `$a = $a % $b`'
+    },
+    {
         'operator': '??',
         'version': '7.2',
-        'name': 'null coalescing'
+        'name': 'null coalescing',
+        'description': 'Hello *world* [test](https://google.com)'
     },
     {
         'operator': '?:',
         'version': '8.0',
-        'name': 'ternary operator'
+        'name': 'ternary operator',
+        'description': 'Test 123'
     }
 ];
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,79 @@
 import React from "https://esm.sh/react";
+import ReactMarkdown from "https://esm.sh/react-markdown";
 import operators from '../operators.ts';
 
 export default function Page() {
+    const [selectedItem, setSelectedItem] = React.useState(-1);
+    const [hideOperators, setHideOperators] = React.useState(false);
+    const [searchText, setSearchText] = React.useState('');
 
-const generatedItems = operators.map((feature) => (
-    <div className="p-2 cursor-pointer">
-        <div className="inline-flex items-center bg-white leading-none text-pink-600 rounded-full p-2 shadow text-teal text-sm">
-            <span className="text-lg inline-flex bg-pink-600 text-white rounded-full h-8 px-5 justify-center items-center">
-                {feature.operator}
-            </span>
-            <span className="inline-flex px-2">
-                {feature.version}
-            </span>
-        </div>
-    </div>
-));
-console.log(generatedItems);
+    const searchResolver = (text: String) => {
+        const foundObject = operators.findIndex((operator) => operator.operator === text);
+        if(foundObject !== -1) {
+            setSelectedItem(foundObject);
+            setHideOperators(true);
+        }
+        else {
+            setSelectedItem(-1);
+            setHideOperators(false);
+        }
+    };
 
-return (
-    <div className="flex flex-col items-center justify-center mt-30">
-        <div className="m-10">
-            <h1 className="text-4xl">PHP Operator Lookup</h1>
-        </div>
-        <div className="text-white font-bold rounded-lg border p-2">
-            <div className="relative text-gray-600">
-                <input type="search" name="search" placeholder="Search" className="bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none" />
-                <button type="submit" className="absolute right-0 top-0 mt-2 mr-3">🔍</button>
+    const generatedItems = operators.map((feature, index) => {
+
+        if(index === selectedItem) {
+            return (
+                <div className="p-2 cursor-pointer" key={index} onClick={() => setSelectedItem(index)}>
+                    <div className="inline-flex items-center bg-purple-600 leading-none text-white rounded-full p-2 shadow text-teal text-sm">
+                        <span className="text-lg inline-flex bg-white text-purple-600 rounded-full h-8 px-5 justify-center items-center">
+                            {feature.operator}
+                        </span>
+                        { feature.version ? <span className="inline-flex px-2">
+                            {feature.version}
+                        </span> : null }
+                    </div>
+                </div>
+            )
+        }
+        else {
+            return (
+                <div className="p-2 cursor-pointer" key={index} onClick={() => setSelectedItem(index)}>
+                    <div className="inline-flex items-center bg-white leading-none text-purple-600 rounded-full p-2 shadow text-teal text-sm">
+                        <span className="text-lg inline-flex bg-purple-600 text-white rounded-full h-8 px-5 justify-center items-center">
+                            {feature.operator}
+                        </span>
+                        { feature.version ? <span className="inline-flex px-2">
+                            {feature.version}
+                        </span> : null }
+                    </div>
+                </div>
+            )
+        }
+    });
+
+    return (
+        <>
+            <div className="flex flex-col h-screen items-center justify-center mt-30">
+                <div className="m-10">
+                    <h1 className="text-4xl">PHP Operator Lookup</h1>
+                </div>
+                <div className="text-white font-bold rounded-lg border p-2">
+                    <div className="relative text-gray-600">
+                        <input onChange={(event) => searchResolver(event.target.value)} type="search" name="search" placeholder="Search" className="bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none" />
+                        <button type="submit" className="absolute right-0 top-0 mt-2 mr-3">🔍</button>
+                    </div>
+                </div>
+                { !hideOperators ? <div className="flex flex-row m-5 flex-wrap mb-auto h-grow">
+                    {generatedItems}
+                </div> : '' }
+                { selectedItem !== -1 ? <div className="rounded-lg border p-5 m-5 w-11/12 max-w-3xl">
+                    <h2 className="text-4xl mb-2">{operators[selectedItem].name}</h2>
+                    {<ReactMarkdown>{operators[selectedItem].description}</ReactMarkdown>}
+                </div> : '' }
+                <div className="block m-2 mb-4 text-center h-10">
+                Built with 💜 by <a className="text-purple-600" href="https://khromov.se" target="_blank">Stanislav Khromov</a> using <a className="text-purple-600" href="https://alephjs.org/" target="_blank">Aleph.js</a> | <a className="text-purple-600" href="/api/operators">API</a> | <a className="text-purple-600" href="https://github.com/khromov/aleph-php-operator-lookup" target="_blank">GitHub</a>
+                </div>
             </div>
-        </div>
-        <div className="flex flex-row m-5">
-            {generatedItems}
-        </div>
-    </div>
-);
+        </>
+    );
 }
